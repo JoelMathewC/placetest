cb0ea6825ff606b5bf0a46f52123c59f8fc42566
JoelMathewC <joelmathewcherian@gmail.com>
2023-02-08 00:27:31 +0530

    New Design

--- test.py
+++ test.py
@@ -2,3 +2,20 @@ from turtle import *
 import random
 Screen()
 Turtle()
+
+bgcolor('black') 
+x = 1 
+speed(0) 
+while x < 400: 
+  
+ r = random.randint(0,255) 
+ g = random.randint(0,255)  
+ b = random.randint(0,255) 
+  
+ colormode(255)  
+ pencolor(r,g,b) 
+ fd(50 + x) 
+ rt(90.991) 
+ x = x+1 
+  
+exitonclick() 
